cd75f865306067f13cafb2dcbc695c7932059f26
sander1 <sander@plexapp.com>
2012-09-29 03:06:17 +0200

    Update to v2.1 framework

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -6,69 +6,73 @@ PMS_URL = 'http://%s/library/sections/'
 ####################################################################################################
 def Start():
 
-	Plugin.AddPrefixHandler('/video/libraryupdater', MainMenu, NAME, ICON, ART)
 	Plugin.AddViewGroup('List', viewMode='List', mediaType='items')
 
-	MediaContainer.art = R(ART)
-	MediaContainer.title1 = NAME
-	MediaContainer.viewGroup = 'List'
-	DirectoryItem.thumb = R(ICON)
-	PopupDirectoryItem.thumb = R(ICON)
+	ObjectContainer.art = R(ART)
+	ObjectContainer.title1 = NAME
+	ObjectContainer.view_group = 'List'
+	DirectoryObject.thumb = R(ICON)
+
+	HTTP.CacheTime = 0
 
 ####################################################################################################
+@handler('/video/libraryupdater', NAME, thumb=ICON, art=ART)
 def MainMenu():
 
-	dir = MediaContainer(noCache=True)
-
+	oc = ObjectContainer(no_cache=True)
 	all_keys = []
 
 	try:
-		sections = XML.ElementFromURL(GetPmsHost(), errors='ignore').xpath('//Directory')
+		sections = XML.ElementFromURL(GetPmsHost()).xpath('//Directory')
+
 		for section in sections:
 			key = section.get('key')
 			title = section.get('title')
-			Log('%s: %s' %(title, key))
-			dir.Append(Function(PopupDirectoryItem(UpdateType, title='Update section "' + title + '"'), title=title, key=[key]))
+			Log(' --> %s: %s' %(title, key))
+
+			oc.add(DirectoryObject(key=Callback(UpdateType, title=title, key=[key]), title='Update section "' + title + '"'))
+
 			all_keys.append(key)
 	except:
-		dir.header = "Couldn't find PMS instance"
-		dir.message = "Add or update the address of PMS in the plugin's preferences"
+		return ObjectContainer(header="Couldn't find PMS instance", message="Add or update the address of PMS in the plugin's preferences")
 
 	if len(all_keys) > 0:
-		dir.Append(Function(PopupDirectoryItem(UpdateType, title='Update all sections'), title='All sections', key=all_keys))
+		oc.add(DirectoryObject(key=Callback(UpdateType, title='All sections', key=all_keys), title='Update all sections'))
 
-	dir.Append(PrefsItem('Preferences', thumb=R('icon-prefs.png')))
+	oc.add(PrefsObject(title='Preferences', thumb=R('icon-prefs.png')))
 
-	return dir
-
-####################################################################################################
-def UpdateType(sender, title, key):
-
-	dir = MediaContainer()
-
-	dir.Append(Function(DirectoryItem(UpdateSection, title='Turbo Scan'), title=title, key=key))
-	dir.Append(Function(DirectoryItem(UpdateSection, title='Deep Scan'), title=title, key=key, deep=True))
-	dir.Append(Function(DirectoryItem(UpdateSection, title='Force Metadata Refresh'), title=title, key=key, force=True))
-
-	return dir
+	return oc
 
 ####################################################################################################
-def UpdateSection(sender, title, key, force=False, deep=False):
+def UpdateType(title, key):
+
+	oc = ObjectContainer(title2=title)
+
+	oc.add(DirectoryObject(key=Callback(UpdateSection, title=title, key=key), title='Turbo Scan'))
+	oc.add(DirectoryObject(key=Callback(UpdateSection, title=title, key=key, deep=True), title='Deep Scan'))
+	oc.add(DirectoryObject(key=Callback(UpdateSection, title=title, key=key, force=True), title='Force Metadata Refresh'))
+
+	return oc
+
+####################################################################################################
+def UpdateSection(title, key, force=False, deep=False):
 
 	for section in key:
 		url = GetPmsHost() + section + '/refresh'
+
 		if force:
 			url += '?force=1'
 		elif deep:
 			url += '?deep=1'
-		update = HTTP.Request(url, cacheTime=1).content
+
+		update = HTTP.Request(url, cacheTime=0).content
 
 	if title == 'All sections':
-		return MessageContainer(title, 'All sections will be updated!')
+		return ObjectContainer(header=title, message='All sections will be updated!')
 	elif len(key) > 1:
-		return MessageContainer(title, 'All chosen sections will be updated!')
+		return ObjectContainer(header=title, message='All chosen sections will be updated!')
 	else:
-		return MessageContainer(title, 'Section "' + title + '" will be updated!')
+		return ObjectContainer(header=title, message='Section "' + title + '" will be updated!')
 
 ####################################################################################################
 def GetPmsHost():
